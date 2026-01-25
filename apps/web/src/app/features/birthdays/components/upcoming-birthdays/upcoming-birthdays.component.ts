810ca9d3483f5203645cb1a4ee33f39e08dcc685
Alexander Topliyski <aleksandar.topliyski@dskbank.bg>
2026-01-25 03:34:44 +0200

    Add selected to the upcoming bds

--- apps/web/src/app/features/birthdays/components/upcoming-birthdays/upcoming-birthdays.component.ts
+++ apps/web/src/app/features/birthdays/components/upcoming-birthdays/upcoming-birthdays.component.ts
@@ -1,6 +1,7 @@
 import { TitleCasePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { BdIconComponent } from '@bd-only/bd-icons';
+import { Birthday } from '@birthday-app/shared';
 import { BirthdayActionCardComponent } from '../birthday-action-card/birthday-action-card.component';
 
 @Component({
@@ -11,5 +12,8 @@ import { BirthdayActionCardComponent } from '../birthday-action-card/birthday-ac
   styleUrls: ['./upcoming-birthdays.component.scss'],
 })
 export class UpcomingBirthdaysComponent {
-  readonly birthdays = input<any[]>([]);
+  readonly birthdays = input<Birthday[]>([]);
+  readonly selectedBirthdayId = input.required<string | null>();
+
+  readonly cardClick = output<string | null>();
 }
